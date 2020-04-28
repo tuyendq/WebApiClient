a49c5715a6500dc51de43aa55b40b3b857ffd0aa
Tuyen Dang <tuyendq@gmail.com>
2020-04-28 16:14:24 +0700

    Processing the Json Result

--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Net.Http;
 using System.Net.Http.Headers;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace WebApiClient
@@ -22,10 +24,24 @@ namespace WebApiClient
                 new MediaTypeWithQualityHeaderValue("application/vnd.github.v3+json"));
             client.DefaultRequestHeaders.Add("User-Agent", ".NET Foundation Repository Reporter");
 
-            var stringTask = client.GetStringAsync("https://api.github.com/orgs/dotnet/repos");
+            var url = "https://api.github.com/orgs/dotnet/repos";
+            // url = "https://api.ipify.org";
 
-            var msg = await stringTask;
-            System.Console.WriteLine(msg);
+            // var stringTask = client.GetStringAsync("https://api.github.com/orgs/dotnet/repos");
+            // var msg = await stringTask;
+            // var msg = await client.GetStringAsync(url);
+            // System.Console.WriteLine(msg);
+
+            var streamTask = client.GetStreamAsync(url);
+            var repositories = await JsonSerializer.DeserializeAsync<List<Repository>>(await streamTask);
+
+            foreach (var repo in repositories)
+            {
+                System.Console.WriteLine(repo.name);
+            }
+
+
+            
         }
     }
 }
