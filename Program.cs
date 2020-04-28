a8daae5460b72adb018aab323fc92997827e17cb
Tuyen Dang <tuyendq@gmail.com>
2020-04-28 15:56:42 +0700

    Making web request

--- Program.cs
+++ Program.cs
@@ -1,12 +1,31 @@
 ﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
 
 namespace WebApiClient
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
             Console.WriteLine("\nWeb API Client\n\n");
+
+            await ProcessRepositories();
+        }
+
+        private static readonly HttpClient client = new HttpClient();
+        private static async Task ProcessRepositories()
+        {
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/vnd.github.v3+json"));
+            client.DefaultRequestHeaders.Add("User-Agent", ".NET Foundation Repository Reporter");
+
+            var stringTask = client.GetStringAsync("https://api.github.com/orgs/dotnet/repos");
+
+            var msg = await stringTask;
+            System.Console.WriteLine(msg);
         }
     }
 }
